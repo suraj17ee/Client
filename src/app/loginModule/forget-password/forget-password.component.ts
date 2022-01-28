8da35100c35d4100943c37399b5c38f72f6e0ae5
Shyam Dhage <shyamdhage000@gmail.com>
2022-01-28 17:55:05 +0530

    forgetpasswordform design

--- src/app/loginModule/forget-password/forget-password.component.ts
+++ src/app/loginModule/forget-password/forget-password.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-forget-password',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent implements OnInit {
+  forgetpassForm: FormGroup;
+  
 
-  constructor() { }
+
+  constructor(fb: FormBuilder) {
+    this.forgetpassForm = fb.group({
+      email : new FormControl('',[Validators.required]),
+    });
+  }
+
+  get getformControl(){
+    return this.forgetpassForm.controls
+  }
 
   ngOnInit(): void {
   }
+  displayStyle = "none";
+  submitForm(){
+    const userLoginData = {
+      email : this.forgetpassForm.get('email')?.value,
+      
+    };
+  }
+  cancelForm(){
+    this.forgetpassForm.reset()
+  }
 
+ 
 }
