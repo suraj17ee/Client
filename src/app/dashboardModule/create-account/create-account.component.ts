4a21538d56f37d8921f05655ac087cb7eb9daebc
Shyam Dhage <shyamdhage000@gmail.com>
2022-01-31 11:01:28 +0530

    create-account form designed

--- src/app/dashboardModule/create-account/create-account.component.ts
+++ src/app/dashboardModule/create-account/create-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  createaccountForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.createaccountForm = fb.group({
+      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      accountType : new FormControl('',[Validators.required]),
+      balance:new FormControl('',[Validators.required]),
+      
+    });
+  }
+
+  get getaccountFormControls(){
+    return this.createaccountForm.controls
+  }
 
   ngOnInit(): void {
   }
 
+  submitForm(){
+    const userLoginData = {
+      email : this.createaccountForm.get('email')?.value,
+      accountType : this.createaccountForm.get('accountType')?.value,
+      balance:this.createaccountForm.get('balance')?.value,
+    };
+    
+  }
+
+  cancelForm(){
+    this.createaccountForm.reset()
+  }
 }
