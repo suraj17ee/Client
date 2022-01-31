936842108c0e8970eb438a0072b7c015cbb18317
SURAJ <suraj17ee@gmail.com>
2022-01-31 15:40:56 +0530

    home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAccountComponent } from './dashboardModule/create-account/create-account.component';
 import { DashboardComponent } from './dashboardModule/dashboard/dashboard.component';
+import { HomeComponent } from './dashboardModule/home/home.component';
+import { ProfileComponent } from './dashboardModule/profile/profile.component';
 import { ForgetPasswordComponent } from './loginModule/forget-password/forget-password.component';
 import { SetPasswordComponent } from './loginModule/set-password/set-password.component';
 import { SigninComponent } from './loginModule/signin/signin.component';
@@ -13,8 +15,10 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'forget-password', component: ForgetPasswordComponent },
   { path: 'set-password', component: SetPasswordComponent },
-  { path: 'create-account', component:CreateAccountComponent },
+  // { path: 'create-account', component:CreateAccountComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', component: SigninComponent },
  
 ];
