7470dcf2837e02d0531fb0c2b2c6f281ea457e66
Shubham Gawade <53399732+Shubham-Gawade@users.noreply.github.com>
2022-02-01 15:16:50 +0530

    Routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateAccountComponent } from './dashboardModule/create-account/create-account.component';
 import { DashboardComponent } from './dashboardModule/dashboard/dashboard.component';
 import { HomeComponent } from './dashboardModule/home/home.component';
 import { ProfileComponent } from './dashboardModule/profile/profile.component';
@@ -15,12 +14,10 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'forget-password', component: ForgetPasswordComponent },
   { path: 'reset-password/:token', component: SetPasswordComponent },
-  { path: 'create-account', component:CreateAccountComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'home', component: HomeComponent },
   { path: '**', component: SigninComponent },
- 
 ];
 
 @NgModule({
