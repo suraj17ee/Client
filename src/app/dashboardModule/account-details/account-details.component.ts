b4771b235f2d491421776406f01c3637d37064b0
Shyam Dhage <shyamdhage000@gmail.com>
2022-02-03 11:23:32 +0530

    account created

--- src/app/dashboardModule/account-details/account-details.component.ts
+++ src/app/dashboardModule/account-details/account-details.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from 'src/app/services/account.service';
-import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-account-details',
@@ -11,53 +7,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./account-details.component.css']
 })
 export class AccountDetailsComponent implements OnInit {
-
-  createAccount: FormGroup;
-  iscreated:Boolean=false;
-  message : String = "";
-
-
-  constructor(fb: FormBuilder, private accountService: AccountService,private router: Router) {
-    this.createAccount = fb.group({
-      
-      accountType : new FormControl('',[Validators.required]),
-      balance:new FormControl('',[Validators.required]),
-      
-    });
+  
+    userid:number=8;
+   rowData:any=[];
+  constructor(private accountService: AccountService) {
   }
-
-  get getaccountFormControls(){
-    return this.createAccount.controls
-  }
+  columnDefs:any = [
+		{headerName: 'Account Number', field: 'account_Id' },
+		{headerName: 'Account Type', field: 'accountType'},
+    {headerName: 'Date Created', field: 'dateCreated'},
+    {headerName: 'Account Balance', field: 'balance'},
+    {headerName: 'Account Status', field: 'accountStatus'}
+	];
+  
 
   ngOnInit(): void {
-  }
-
-  submitForm(){
-    const accountData = {
-      accountType : this.createAccount.get('accountType')?.value,
-      balance:this.createAccount.get('balance')?.value,
-      dateCreated:"10/10/2022",
-    };
     
-    this.accountService.saveAccount(accountData).subscribe((response:any) => {
-      if(response==201){
-        this.message=response;
-        this.router.navigate(['/dashboard'])
-      }
-      else{
-        this.message=response;
-      }
-    },
-      (error: any) => {
-        console.log(error)
-      }
-    );
-
-
   }
+  getData(){
+    const data={
+      userId:this.userid
+    }
+    this.accountService.getAccounts(data).subscribe((res)=>{
+      //this.rowData = res;
+     console.log(this.rowData);
+      console.log(res[0]);
+    }  );
 
-  cancelForm(){
-    this.createAccount.reset()
+    console.log("caling");
   }
+ 
+ 
 }
