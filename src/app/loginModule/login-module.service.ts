7675d698f9b6b46c9610d4644359a8575c348cad
Shubham Gawade <53399732+Shubham-Gawade@users.noreply.github.com>
2022-02-03 11:28:24 +0530

    Update profile done

--- src/app/loginModule/login-module.service.ts
+++ src/app/loginModule/login-module.service.ts
@@ -7,6 +7,7 @@ import { HttpClient } from "@angular/common/http";
 })
 export class LoginModuleService {
   constructor(private http: HttpClient) {}
+  loggedIn : boolean =  false;
 
   saveUser(user: any): Observable<any> {
     return this.http.post("http://localhost:8080/server/save", user);
--- src/app/mainModule/header/header.component.ts
+++ src/app/mainModule/header/header.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
-
   userId: any;
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.userId=localStorage.getItem("userId")
+    this.userId = localStorage.getItem("userId");
   }
 
-  logout(){
-    localStorage.removeItem("userId")
+  logout() {
+    localStorage.removeItem("userId");
     this.router.navigate(["/login"]);
   }
 }
