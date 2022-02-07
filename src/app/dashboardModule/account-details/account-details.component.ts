4dadb3d7a0d7c6d48de3a6e8e087ea4cf037d9c6
Shubham Gawade <53399732+Shubham-Gawade@users.noreply.github.com>
2022-02-07 17:26:00 +0530

    ag grid issue solved

--- src/app/dashboardModule/account-details/account-details.component.ts
+++ src/app/dashboardModule/account-details/account-details.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from "@angular/core";
-import { AccountService } from "src/app/services/account.service";
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
-  selector: "app-account-details",
-  templateUrl: "./account-details.component.html",
-  styleUrls: ["./account-details.component.css"],
+  selector: 'app-account-details',
+  templateUrl: './account-details.component.html',
+  styleUrls: ['./account-details.component.css'],
 })
 export class AccountDetailsComponent implements OnInit {
   userid: number | undefined;
   accounts: any = [];
 
   columnDefs = [
-    { headerName: "Account ID", field: "accountId" },
-    { headerName: "Account Status", field: "accountStatus" },
-    { headerName: "Account Type", field: "accountType" },
-    { headerName: "Balance", field: "balance" },
-    { headerName: "Account Creation Date", field: "dateCreated" },
+    { headerName: 'Account ID', field: 'accountId' },
+    { headerName: 'Account Status', field: 'accountStatus' },
+    { headerName: 'Account Type', field: 'accountType' },
+    { headerName: 'Balance', field: 'balance' },
+    { headerName: 'Account Creation Date', field: 'dateCreated' },
   ];
   constructor(private accountService: AccountService) {}
 
@@ -24,7 +24,7 @@ export class AccountDetailsComponent implements OnInit {
   }
 
   getData() {
-    var userID: number = Number(localStorage.getItem("userId"));
+    var userID: number = Number(localStorage.getItem('userId'));
 
     this.accountService.getAccounts(userID).subscribe((res) => {
       this.accounts = res;
