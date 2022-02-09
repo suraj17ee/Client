ed702eb715348f170270fa72393403ab06e6ddee
Shubham Gawade <53399732+Shubham-Gawade@users.noreply.github.com>
2022-02-10 00:10:51 +0530

    Loader added

--- src/app/dashboardModule/fund-transfer/fund-transfer.component.ts
+++ src/app/dashboardModule/fund-transfer/fund-transfer.component.ts
@@ -9,6 +9,7 @@ import { AccountService } from 'src/app/services/account.service';
 import { FundtransferService } from 'src/app/services/fundtransfer.service';
 import { Router } from '@angular/router';
 import { NotificationService } from 'src/app/services/notification.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-fund-transfer',
@@ -17,25 +18,27 @@ import { NotificationService } from 'src/app/services/notification.service';
 })
 export class FundTransferComponent implements OnInit {
   fundTransferForm: FormGroup;
-  iscreated: Boolean = false;
+  checkZeroAmount: Boolean = false;
   accounts: any = [];
+  checkZeroOTP: boolean = false;
 
   constructor(
     fb: FormBuilder,
     private fundtransferService: FundtransferService,
     private router: Router,
     private accountService: AccountService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private SpinnerService: NgxSpinnerService
   ) {
     this.fundTransferForm = fb.group({
       fromAccount: new FormControl('', Validators.required),
       toAccount: new FormControl('', Validators.required),
       description: new FormControl('', [Validators.required]),
-      balance: new FormControl('', [Validators.required]),
+      amount: new FormControl('', [Validators.required]),
       otp: new FormControl('', [Validators.required]),
     });
   }
-  get getaccountFormControls() {
+  get getAccountFormControls() {
     return this.fundTransferForm.controls;
   }
 
@@ -49,33 +52,44 @@ export class FundTransferComponent implements OnInit {
     const transferData = {
       userId: userId,
       toAccount: this.fundTransferForm.get('toAccount')?.value,
-      amount: this.fundTransferForm.get('balance')?.value,
+      amount: this.fundTransferForm.get('amount')?.value,
       description: this.fundTransferForm.get('description')?.value,
       fromAccount: this.fundTransferForm.get('fromAccount')?.value,
       otp: Number(this.fundTransferForm.get('otp')?.value),
     };
-    this.fundtransferService.transfer(transferData).subscribe(
-      (response: any) => {
-        if (response.statusCode == 201) {
-          this.notificationService.createNotification(
-            'success',
-            'Success',
-            response.message
-          );
-          this.router.navigate(['/dashboard/account-details']);
-        } else if (response.statusCode == 400) {
-          this.notificationService.createNotification(
-            'error',
-            'Error',
-            response.message
-          );
-          this.router.navigate(['/dashboard/fund-transfer']);
+
+    if (this.fundTransferForm.get('amount')?.value == 0) {
+      this.checkZeroAmount = true;
+    } else if (this.fundTransferForm.get('otp')?.value == 0) {
+      this.checkZeroOTP = true;
+    } else {
+      this.fundtransferService.transfer(transferData).subscribe(
+        (response: any) => {
+          if (response.statusCode == 201) {
+            this.SpinnerService.show();
+            setTimeout(() => {
+              this.SpinnerService.hide();
+            }, 10000);
+            this.notificationService.createNotification(
+              'success',
+              'Success',
+              response.message
+            );
+            this.router.navigate(['/dashboard/account-details']);
+          } else if (response.statusCode == 400) {
+            this.notificationService.createNotification(
+              'error',
+              'Error',
+              response.message
+            );
+            this.router.navigate(['/dashboard/fund-transfer']);
+          }
+        },
+        (error: any) => {
+          console.log(error);
         }
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
+      );
+    }
   }
 
   cancelForm() {
@@ -83,9 +97,11 @@ export class FundTransferComponent implements OnInit {
   }
 
   getOTP() {
+    this.SpinnerService.show();
     var userId: number = Number(localStorage.getItem('userId'));
 
     this.fundtransferService.getOTP(userId).subscribe((response) => {
+      this.SpinnerService.hide();
       if (response.statusCode == 201) {
         this.notificationService.createNotification(
           'success',
