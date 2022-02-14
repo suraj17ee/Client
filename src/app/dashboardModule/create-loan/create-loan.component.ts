a569337673fa3c6c265c792798ed85b7008271bf
Shubham Gawade <53399732+Shubham-Gawade@users.noreply.github.com>
2022-02-14 15:45:24 +0530

    loan cmp created

--- src/app/dashboardModule/create-loan/create-loan.component.ts
+++ src/app/dashboardModule/create-loan/create-loan.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-loan',
   templateUrl: './create-loan.component.html',
-  styleUrls: ['./create-loan.component.css']
+  styleUrls: ['./create-loan.component.css'],
 })
 export class CreateLoanComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
