f3ea80dd646c4709f042fcb0fe02afcd2c7ccf74
Shyam Dhage <shyamdhage000@gmail.com>
2022-02-15 18:25:49 +0530

    loan modified

--- src/app/dashboardModule/create-loan/create-loan.component.ts
+++ src/app/dashboardModule/create-loan/create-loan.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { NotificationService } from 'src/app/services/notification.service';
 import { LoanService } from 'src/app/services/loan.service';
@@ -10,34 +15,33 @@ import { LoanService } from 'src/app/services/loan.service';
   styleUrls: ['./create-loan.component.css'],
 })
 export class CreateLoanComponent implements OnInit {
-  createLoanForm: FormGroup;
-  purpose: string | undefined;
-  interestRate: number = 0;
-  tenure: number = 0;
-  amount: number = 0;
-  monthlyEMI: number = 0;
-
-  purposes: Array<any> = [
-    { id: 0, name: 'Personal' },
-    { id: 1, name: 'Home' },
-    { id: 2, name: 'Vehicle' },
-  ];
-  tenures: Array<any> = [
-    { id: 0, name: '3 Months' },
-    { id: 1, name: '6 Months' },
-    { id: 2, name: '12 Months' },
-  ];
+  createLoanForm!: FormGroup;
+  loanPurpose!:string;
+  interestRate!:number;
+  tenure!:number;
+  monthlyEMI:number=0;
+  purposes:Array<any> = [
+    {id: 0, name: "Personal"},
+    {id: 1, name: "Home"},
+    {id: 2, name: "Vehicle"}
+];
+tenures:Array<any> = [
+  {id: 0, name: "3 Months"},
+  {id: 1, name: "6 Months"},
+  {id: 2, name: "12 Months"}
+];
 
   constructor(
+    fb: FormBuilder,
     private loanService: LoanService,
     private router: Router,
     private notificationService: NotificationService
   ) {
     this.createLoanForm = new FormGroup({
-      loanAmount: new FormControl('', [Validators.required]),
-      loanPurpose: new FormControl('', [Validators.required]),
-      tenureInMonths: new FormControl('', [Validators.required]),
-      accountId: new FormControl('', [Validators.required]),
+      purpose:new FormControl('', [Validators.required]),
+      amount:new FormControl('', [Validators.required]),
+      tenure: new FormControl('', [Validators.required]),
+      account: new FormControl('', [Validators.required]),
     });
   }
 
@@ -46,59 +50,53 @@ export class CreateLoanComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-
-  updateInterestRate() {
-    if (this.createLoanForm.get('loanPurpose')?.value.id == 0) {
-      this.interestRate = 7;
-      this.purpose = 'Personal';
-    } else if (this.createLoanForm.get('loanPurpose')?.value.id == 1) {
-      this.interestRate = 9;
-      this.purpose = 'Home';
-    } else {
-      this.interestRate = 12;
-      this.purpose = 'Vehicle';
-    }
-    console.log(this.interestRate);
+  updateInterestRate(){
+      let purpose = this.createLoanForm.get('purpose')?.value;
+      if(purpose.id ==0 ){
+        this.interestRate=7;
+        this.loanPurpose="Personal";
+      }else if(purpose.id ==1){
+        this.interestRate=9;
+        this.loanPurpose="Home";
+      }else{
+        this.interestRate=12;
+        this.loanPurpose="Vehicle";
+      }
+      console.log(this.interestRate);
   }
-
-  updateInterest() {
-    if (this.createLoanForm.get('tenureInMonths')?.value.id == 0) {
-      this.tenure = 3;
-    } else if (this.createLoanForm.get('tenureInMonths')?.value.id == 1) {
-      this.tenure = 6;
-    } else {
-      this.tenure = 12;
-    }
-    console.log(this.tenure);
+  updateInterest(){
+    let tenure = this.createLoanForm.get('tenure')?.value;
+      if(tenure.id ==0 ){
+        this.tenure=3;
+      }else if(tenure.id ==1){
+        this.tenure=9;
+      }else{
+        this.tenure=12;
+      }
+      console.log(this.tenure);
   }
-
-  calculateEMI() {
-    this.amount = this.createLoanForm.get('loanAmount')?.value;
-    if (this.amount >= 1000) {
-      this.monthlyEMI =
-        (this.amount + (this.amount * this.interestRate) / 100) / this.tenure;
-      this.monthlyEMI =
-        Math.round((this.monthlyEMI + Number.EPSILON) * 100) / 100;
+  calculateEMI(){
+    var amount= this.createLoanForm.get('amount')?.value;
+    if(amount>=1000){
+      this.monthlyEMI = (amount+(amount *this.interestRate/100 ))/this.tenure;
+      this.monthlyEMI = Math.round((this.monthlyEMI + Number.EPSILON) * 100) / 100
+      console.log(this.monthlyEMI);
     }
-    console.log(this.monthlyEMI);
+    console.log(this.monthlyEMI)
   }
-
-  createLoan() {
+  createLoan(){
     var userId: number = Number(localStorage.getItem('userId'));
 
     const loanData = {
-      userId: userId,
-      accountId: this.createLoanForm.get('accountId')?.value,
-      loanAmount: this.createLoanForm.get('loanAmount')?.value,
-      remainingAmount:
-        this.createLoanForm.get('loanAmount')?.value +
-        this.interestRate * this.tenure,
-      loanPurpose: this.purpose,
-      interest: this.interestRate,
-      tenureInMonths: this.tenure,
-      monthlyEMI: this.monthlyEMI,
+      userId:userId,
+      accountId: this.createLoanForm.get('account')?.value,
+      balance:this.createLoanForm.get('amount')?.value,
+      loanType:this.loanPurpose,
+      tenure: this.tenure,
+      monthlyEMI :this.monthlyEMI
     };
 
+   
     this.loanService.createLoan(loanData).subscribe(
       (response: any) => {
         if (response.statusCode == 201) {
@@ -114,11 +112,12 @@ export class CreateLoanComponent implements OnInit {
             'Error',
             response.message
           );
+          this.router.navigate(['/create-loan']);
         }
       },
       (error: any) => {
         console.log(error);
       }
     );
-  }
+   }
 }
